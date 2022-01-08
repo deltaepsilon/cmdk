dbf39a77621ef04aed322da536ea5a1568663035
Chris Esplin <chris@chrisesplin.com>
2022-01-08 11:30:09 -0700

    Another attempt

--- apps/external/webpack.config.js
+++ apps/external/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
-const withTM = require('next-transpile-modules')(['command-k', 'ui']);
 // See https://github.com/iykekings/react-swc-loader-template
+
 const config = {
   mode: 'development',
   entry: './index.tsx',
@@ -9,20 +9,15 @@ const config = {
       {
         test: /\.(ts|tsx)$/,
         loader: 'swc-loader',
-        // include: [
-        //   path.resolve(__dirname, './index.tsx'),
-        //   path.resolve(__dirname, '../../packages/command-k'),
-        // ],
-        // exclude: /node_modules/,
+        include: [
+          path.resolve(__dirname),
+          path.resolve(__dirname, '../../packages/command-k'),
+          path.resolve(__dirname, '../../packages/ui'),
+        ],
+        exclude: /node_modules/,
       },
     ],
   },
 };
-// const { webpack } = withTM();
-// const webpackResult = webpack();
-
-// console.log({ webpack, webpackResult });
-
-console.log(JSON.stringify(config, null, 2));
 
 module.exports = config;
